f7a01c1be52667a0d277392f36e8bf70b446c5e2
Jesús López <jesus.lopezg18@tectijuana.edu.mx>
2023-04-20 12:12:23 -0700

    diseño del error 404 y funcionalidad TERMINADA

--- 404error.php
+++ 404error.php
@@ -1,1 +1,31 @@
-<h1>ERROR 404</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel="shortcut icon" href="favicon.png">
+        <title>whoops 404</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <script type="text/javascript" src="js\jquery-3.6.4.js"></script>
+        <script type="text/javascript" src="js\bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="fontawesome-free-6.3.0-web\css\all.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Raleway:wght@500&family=Roboto&display=swap" rel="stylesheet">
+    </head>
+    <body>
+          <div class="container text-center">
+            <div class="row">
+                <div class="col-12-sm down">
+                    <i class="fa-solid fa-robot great"></i>           
+                    <h2 style="font-size:80px; color: #0466C8;"> 404 </h2>
+                    <h1>OOPS! ERROR </h1>
+                    <p>La página que estabas buscando parece que ya no existe, fue removida o tal vez solamente escribiste mal el enlace, prueba utilizar el boton de abajo para regresar al inicio</p>
+                    <a href="index.php"><button class="btn buttonColorin" type="submit">Llévame al inicio</button></a>
+                </div>    
+            </div>
+        </div>
+    </body>
+</html>
